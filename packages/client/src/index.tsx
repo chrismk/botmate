11f26e45441b011639aa7b8e1b2d6b6df9145dea
xencodes <xencodes@icloud.com>
2021-09-17 15:58:18 +0530

    ui: add theme support

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import App from './App'
+import theme from './config/theme'
 
 ReactDOM.render(
 	<React.StrictMode>
-		<ChakraProvider>
+		<ChakraProvider theme={theme}>
 			<App />
 		</ChakraProvider>
 	</React.StrictMode>,
