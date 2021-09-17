d76f12b36c7d4c4b8e9a12cfb52db0ddea2c7b73
xencodes <xencodes@icloud.com>
2021-09-17 16:00:28 +0530

    ui: add fonts

--- packages/client/src/config/theme.ts
+++ packages/client/src/config/theme.ts
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
+	fonts: {
+		heading: 'Open Sans',
+		body: 'Roboto',
+	},
 	colors: {
 		brand: {
 			'50': '#ede7f7',
