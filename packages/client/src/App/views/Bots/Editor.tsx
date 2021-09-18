ea2bee3ae8e67a65abccdc03dacd01d65f20ae59
xencodes <xencodes@icloud.com>
2021-09-18 23:01:06 +0530

    feat: add modules card

--- packages/client/src/App/views/Bots/Editor.tsx
+++ packages/client/src/App/views/Bots/Editor.tsx
@@ -1,9 +1,11 @@
-import { Box } from '@chakra-ui/layout'
-import { useLocation } from 'react-router'
-
-import UIBreadcrumb from 'components/ui/breadcrumb'
+import { SimpleGrid, GridItem, Stack } from '@chakra-ui/layout'
+import { Redirect, useLocation } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+
 import { Bot } from 'atom'
+import BotControl from 'components/bots/controls'
+import UIBreadcrumb from 'components/ui/breadcrumb'
+import { UICard } from 'components/ui/card'
 
 const BotEditor: React.FC = (props) => {
 	const { t } = useTranslation()
@@ -14,14 +16,24 @@ const BotEditor: React.FC = (props) => {
 	}
 
 	return (
-		<Box>
+		<Stack spacing={4}>
 			<UIBreadcrumb
 				steps={[
 					{ name: t('common.bots'), link: '/bots' },
 					{ name: bot.name, link: '' },
 				]}
 			/>
-		</Box>
+
+			<SimpleGrid columns={5} spacing={4}>
+				<Stack as={GridItem} colSpan={{ base: 5, md: 2 }}>
+					<BotControl />
+
+					<UICard title={t('common.modules')}></UICard>
+				</Stack>
+
+				<GridItem colSpan={{ base: 5, md: 3 }}></GridItem>
+			</SimpleGrid>
+		</Stack>
 	)
 }
 
