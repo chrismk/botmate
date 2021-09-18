2d8f0348df7c4e5b2bf5c8cab4b736f39be000ba
xencodes <xencodes@icloud.com>
2021-09-18 22:11:26 +0530

    style(settings): responsive grid

--- packages/client/src/App/views/Settings.tsx
+++ packages/client/src/App/views/Settings.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@chakra-ui/layout'
+import { Stack, SimpleGrid } from '@chakra-ui/layout'
 
 import Languages from 'components/settings/languages'
 import ColorSwticher from 'components/settings/color-switcher'
@@ -6,8 +6,10 @@ import ColorSwticher from 'components/settings/color-switcher'
 const Settings: React.FC = () => {
 	return (
 		<Stack>
-			<Languages />
-			<ColorSwticher />
+			<SimpleGrid columns={{ base: 1, '2xl': 2 }} spacing={4}>
+				<Languages />
+				<ColorSwticher />
+			</SimpleGrid>
 		</Stack>
 	)
 }
