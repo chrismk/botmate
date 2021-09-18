037e10489731b3c53a7737c33030b86d1d74c3e8
xencodes <xencodes@icloud.com>
2021-09-18 10:25:11 +0530

    feat: setup framer animation with views

--- packages/client/src/App/index.tsx
+++ packages/client/src/App/index.tsx
@@ -1,13 +1,54 @@
 import React from 'react'
 import { Box, Flex } from '@chakra-ui/react'
+import { Route, Switch, useLocation } from 'react-router'
+import { AnimatePresence, motion } from 'framer-motion'
+
 import AppSidebar from '../components/app/sidebar'
 
+import Home from './views/Home'
+
+interface IViews {
+	path: string
+	view: React.FC
+}
+
+let Views: IViews[] = [
+	{
+		path: '/home',
+		view: Home,
+	},
+]
+
 const App: React.FC = () => {
+	const location = useLocation()
+
 	return (
 		<div>
 			<Flex h='100vh'>
 				<AppSidebar />
-				<Box></Box>
+				<Box>
+					<AnimatePresence exitBeforeEnter initial={false}>
+						<Switch location={location} key={location.pathname}>
+							{Views.map((view, idx) => {
+								return (
+									<Route
+										key={idx}
+										path={view.path}
+										exact
+										component={(props: any) => {
+											const View = view.view
+											return (
+												<motion.div>
+													<View {...props} />
+												</motion.div>
+											)
+										}}
+									/>
+								)
+							})}
+						</Switch>
+					</AnimatePresence>
+				</Box>
 			</Flex>
 		</div>
 	)
