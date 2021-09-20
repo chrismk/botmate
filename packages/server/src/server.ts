3d828478ee94b23b1374144a52e4328c252b776a
xencodes <xencodes@icloud.com>
2021-09-20 15:26:03 +0530

    chore: return client from server

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,22 @@
+import path from 'path'
 import express from 'express'
 import cors from 'cors'
 import http from 'http'
+import env from './env'
 
 const app = express()
 const server = http.createServer(app)
+
+const clientDir = path.join(__dirname, 'bm-client')
+app.use(express.static(clientDir))
+
+app.get('*', (req, res) => {
+	if (env.NODE_ENV === 'development') {
+		res.end('DEVELOPMENT')
+		return
+	}
+	res.sendFile(clientDir + '/index.html')
+})
 
 app.use(cors())
 
