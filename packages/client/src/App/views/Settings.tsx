fda5d37c469eb2fe1d13ca46170659b48c18acbd
xencodes <xencodes@icloud.com>
2021-09-20 15:11:34 +0530

    feat: add donation link

--- packages/client/src/App/views/Settings.tsx
+++ packages/client/src/App/views/Settings.tsx
@@ -2,6 +2,8 @@ import { Stack, SimpleGrid } from '@chakra-ui/layout'
 
 import Languages from 'components/settings/languages'
 import ColorSwticher from 'components/settings/color-switcher'
+import { Button } from '@chakra-ui/button'
+import { HiHeart } from 'react-icons/hi'
 
 const Settings: React.FC = () => {
 	return (
@@ -10,6 +12,21 @@ const Settings: React.FC = () => {
 				<Languages />
 				<ColorSwticher />
 			</SimpleGrid>
+			<a
+				href='https://opencollective.com/botmate'
+				target='_blank'
+				rel='noreferrer'
+			>
+				<Button
+					colorScheme='orange'
+					variant='link'
+					fontWeight='thin'
+					mt={2}
+					leftIcon={<HiHeart />}
+				>
+					Support BotMate development
+				</Button>
+			</a>
 		</Stack>
 	)
 }
