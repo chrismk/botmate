bf062927a0b18714e7290cf0a23760c4c5f0b821
xencodes <xencodes@icloud.com>
2021-09-20 15:20:04 +0530

    feat(welcome): send buttons

--- packages/server/src/modules/welcome/index.ts
+++ packages/server/src/modules/welcome/index.ts
@@ -1,10 +1,16 @@
+import { InlineKeyboard } from 'grammy'
 import { BMContext, BMModule, BMModuleMeta } from '../../types'
 
 const module: BMModule = {
 	handler: (composer) => {
 		composer.on(':new_chat_members', (ctx: BMContext) => {
+			const { buttons } = ctx.session.config
 			const { message = 'Welcome' } = ctx.session.config
-			ctx.reply(message)
+			ctx.reply(message, {
+				reply_markup: {
+					inline_keyboard: buttons,
+				},
+			})
 		})
 	},
 	params: {
